0cd4cd44a8ec6b5bb939dc59d82db24bc29614e0
takuya.suzuki <purelyplus@gmail.com>
2019-04-21 07:51:17 +0000

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_060011) do
+ActiveRecord::Schema.define(version: 2019_04_21_070930) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
